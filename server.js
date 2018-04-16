a10404750e193ecf670372c998149b48340945d8
joyce <joycedelatorre87@gmail.com>
2018-04-16 01:24:10 -0500

    trying to render an image, right now it renders something

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ var app = express();
 var port = 3000;
 
 //set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({defaultLayout: main}));
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 
 var ice_creams =[
-	{iceCream:"Totally Baked"},
+	{iceCream:"Totally Baked",
+	 photo:"/images/totally-baked-bulk-detail.png"
+	},
 	{iceCream:"Coffee Coffee Buzz Buzz"},
 	{iceCream:"chunky Monkey"}
 ]
@@ -27,9 +29,13 @@ app.get("/Coffee", function(req, res){
 });
 
 app.get("/chunky_monkey", function(req, res){
-	res.render ("all-iceCreams"{
+	res.render("index", ice_creams[2]);
+});
+
+app.get("/", function(req, res){
+	res.render ("all-iceCreams",{
 		ices:ice_creams,
-		eater:"Joyce"
+		eater:"Ben & Jerry's"
 	});
 });
 
